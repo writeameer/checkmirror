de0ff9383ec9f3043faf7c9c5dcb5f5a8b5c4629
Alex Pop <al3xpop@gmail.com>
2021-01-21 12:25:13 +0000

    Return json for the api

--- main.go
+++ main.go
@@ -2,97 +2,143 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	_ "github.com/denisenkom/go-mssqldb"
-	//"errors"
 )
 
 func main() {
-
-	// checkEnvironment()
-
-	http.HandleFunc("/", handler)
-	log.Println("Listening on localhost:8282")
-	log.Fatal(http.ListenAndServe(":8282", nil))
+	cfg := getServiceConfig()
+	http.HandleFunc("/", cfg.handler)
+	listenOn := fmt.Sprintf("0.0.0.0:%d", cfg.ListenPort)
+	log.Printf("Listening on %s", listenOn)
+	log.Fatal(http.ListenAndServe(listenOn, nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func (cfg *Config) handler(w http.ResponseWriter, r *http.Request) {
 
 	// Check SQL Role and set 200 if principal
-	status := getStatus()
-	if status != "principal" {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-
-	// Return status
-	fmt.Fprintf(w, status)
-}
-
-func getStatus() (status string) {
-
-	// Set status "down" as default
-	status = "down"
-
-	//connect to the database
-	host := os.Getenv("SQLSERVER_HOST")
-	log.Println("SQL Server host:", host)
-	conn := "sqlserver://" + host + "/?database=master&connection+timeout=30"
-	db, err := sql.Open("sqlserver", conn)
-
-	// Set status "down" for db down
+	mirroring, err := cfg.getMirroringStatus()
 	if err != nil {
 		log.Println(err)
-		status = "down"
+		http.Error(w, JsonErrorIt(err.Error()), http.StatusInternalServerError)
 	} else {
-		// Query DB for role
-		query := "select top 1 mirroring_role_desc from sys.database_mirroring where mirroring_role is not null"
+		if mirroring.OverallMirroringRole != "principal" {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		// Return status
+		fmt.Fprintf(w, PrettyPrintJson(mirroring))
+	}
+}
+
+func (cfg *Config) getMirroringStatus() (mirroring *JsonMirroring, err error) {
+	mirroring = &JsonMirroring{
+		OverallMirroringRole: "none",
+		DatabasesMirroring:   make([]*JsonDbMirroring, 0),
+	}
+	//connect to the database
+	conn := fmt.Sprintf("sqlserver://%s:%d/?database=master&connection+timeout=30", cfg.SqlServerHost, cfg.SqlServerPort)
+	db, err := sql.Open("sqlserver", conn)
+
+	if err != nil {
+		return mirroring, err
+	} else {
+		// Query DB for db names roles
+		query := "SELECT m.name, d.database_id, d.mirroring_role_desc FROM sys.database_mirroring d, sys.databases m WHERE mirroring_role is not null and m.database_id = d.database_id"
 		rows, err := db.Query(query)
 		if err != nil {
-			log.Println(err)
-		}
-		defer rows.Close()
-
-		// Read return result
-		var strrow string
-		for rows.Next() {
-			err = rows.Scan(&strrow)
+			return nil, err
 		}
 
-		// Set status as none
-		if strrow == "" {
-			status = "none"
-		} else if strrow == "PRINCIPAL" {
-			status = "principal"
-		} else if strrow == "MIRROR" {
-			status = "mirror"
+		if rows != nil {
+			defer rows.Close()
+			mirrorRolesMap := make(map[string]struct{}, 0)
+			for rows.Next() {
+				var dbName string
+				var dbId int
+				var dbRole string
+				err = rows.Scan(&dbName, &dbId, &dbRole)
+				if err != nil {
+					return nil, err
+				}
+				dbRole = strings.ToLower(dbRole)
+				dbMirroring := &JsonDbMirroring{
+					Name:          dbName,
+					MirroringRole: dbRole,
+				}
+				if mirroring.OverallMirroringRole == "none" {
+					// store the role of the first db in the overall
+					mirroring.OverallMirroringRole = dbRole
+				} else if mirroring.OverallMirroringRole == "principal" && dbRole != "principal" {
+					mirroring.OverallMirroringRole = dbRole
+				}
+				mirroring.DatabasesMirroring = append(mirroring.DatabasesMirroring, dbMirroring)
+				mirrorRolesMap[dbRole] = struct{}{}
+			}
+
+			// multiple db mirroring roles result in a "mixed" overall mirroring role
+			if len(mirrorRolesMap) > 1 {
+				mirroring.OverallMirroringRole = "mixed"
+			}
 		}
 	}
-
-	log.Println("I am: " + status)
-	return status
+	log.Println("I am: " + mirroring.OverallMirroringRole)
+	return mirroring, nil
 }
 
-// Ensures all env var for running this are defined
-func checkEnvironment() {
-
-	envvars := []string{
-		"SQLSERVER_HOST",
-		"SQLSERVER_PORT",
-		"LISTEN_PORT",
+func PrettyPrintJson(v interface{}) string {
+	outBytes, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		log.Printf("ERROR: PrettyPrintJson error '%s' for v='%+v'", err.Error(), v)
 	}
-
-	checkEnvironmentVariable(envvars)
+	outBytes = append(outBytes, "\n"...)
+	return fmt.Sprintf("%-512s", string(outBytes)) // Pad right to avoid "Friendly HTTP error messages" issue in IE & Chrome
 }
 
-// Checks is env va exists, else logs fatal
-func checkEnvironmentVariable(envvars []string) {
-	for _, envvar := range envvars {
-		if os.Getenv(envvar) == "" {
-			log.Fatal("Error, environent vaiable " + envvar + " was not defined. Exitting.")
-		}
+func JsonErrorIt(msg string) string {
+	jsonErr := JsonResponse{
+		Error: msg,
 	}
+	outBytes, err := json.MarshalIndent(jsonErr, "", "  ")
+	if err != nil {
+		log.Printf("ERROR: JsonErrorIt marshal error '%s'", err.Error())
+	}
+	return fmt.Sprintf("%-512s", outBytes) // Pad right to avoid "Friendly HTTP error messages" issue in IE & Chrome
+}
+
+func getServiceConfig() (cfg *Config) {
+	cfg = &Config{
+		SqlServerHost: "127.0.0.1",
+		SqlServerPort: 1433,
+		ListenPort:    8282,
+	}
+
+	if sqlHost := os.Getenv("SQLSERVER_HOST"); sqlHost != "" {
+		cfg.SqlServerHost = os.Getenv("SQLSERVER_HOST")
+	}
+
+	if sqlPort := os.Getenv("SQLSERVER_PORT"); sqlPort != "" {
+		intPort, err := strconv.Atoi(sqlPort)
+		if err != nil {
+			log.Fatalf("Provided value for SQLSERVER_PORT, %s, cannot be converted to an integer", sqlPort)
+		}
+		cfg.SqlServerPort = uint16(intPort)
+	}
+
+	if listenPort := os.Getenv("LISTEN_PORT"); listenPort != "" {
+		intPort, err := strconv.Atoi(listenPort)
+		if err != nil {
+			log.Fatalf("Provided value for LISTEN_PORT, %s, cannot be converted to an integer", listenPort)
+		}
+		cfg.ListenPort = uint16(intPort)
+	}
+
+	return cfg
 }
